8fd6227c74c35956398910f76dfbb0d4ecbc632e
Peyton Casper <peytoncas@gmail.com>
2015-06-11 23:34:11 -0500

    Flipped the controls Around
    Flipped the way controls were handled so it doesnt have to connect to
    the sphero for each button press. Rather its a stream and simply sends a
    command

--- SpheroFloorPlan/SpheroFloorPlan/app.js
+++ SpheroFloorPlan/SpheroFloorPlan/app.js
@@ -1,44 +1,42 @@
 ﻿"use strict";
 
 var sphero = require("sphero");
-var orb = sphero("COM9");
+var orb = sphero("COM4");
 
 
 var stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.resume();
 stdin.setEncoding('utf8');
+orb.connect(function () {
+    stdin.on('data', function (key) {
+        var degree = -1;
+        switch (key) {
+            case 'w':
+                console.log("Foward");
+                degree = 0;
+                break;
+            case 'a':
+                console.log("Left");
+                degree = 270;
+                break;
+            case 's':
+                console.log("Backwards");
+                degree = 180;
+                break;
+            case 'd':
+                console.log("Right");
+                degree = 90;
+                break;
+            case '\u0003':
+                process.exit();
+                break;
+            default:
+                break;
+        }
+        if (degree != -1) {
+            orb.roll(10, degree);
 
-stdin.on('data', function (key) {
-    degree = -1;
-    switch (key) {
-        case 'w':
-            console.log("Foward");
-            degree = 0;
-            break;
-        case 'a':
-            console.log("Left");
-            degree = 270;
-            break;
-        case 's':
-            console.log("Backwards");
-            degree = 180;
-            x -= 1;
-            break;
-        case 'd':
-            console.log("Right");
-            degree = 90;
-            break;
-        case '\u0003':
-            process.exit();
-            break;
-        default:
-            break;
-    }
-    
-    if (degree != -1) {
-        orb.connect(function () {
-                orb.roll(5, degree);
-        });
-    }
+        }
+    });
 });
